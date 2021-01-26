0f9bbaa7db0aceccb521bf9007f658e663f5284a
srtidd176 <riverstone98@gmail.com>
2021-01-26 11:43:13 -0500

    spark config

--- build.sbt
+++ build.sbt
@@ -2,4 +2,6 @@ name := "Zillow-Analysis"
 
 version := "0.1"
 
-scalaVersion := "2.13.4"
+scalaVersion := "2.12.10"
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.0.1"
--- src/main/scala/zillow/DataFrameBuilder.scala
+++ src/main/scala/zillow/DataFrameBuilder.scala
@@ -1,5 +1,12 @@
 package zillow
 
+import org.apache.spark.sql.SparkSession
+
 object DataFrameBuilder {
+
+  val spark = SparkSession.builder
+    .appName("DataFrameBuilder")
+    .master("local[4]")
+    .getOrCreate()
 
 }
