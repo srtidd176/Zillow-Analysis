573b2cd62c611949b62d37f31ff09e5191fa2c35
srtidd176 <riverstone98@gmail.com>
2021-01-26 12:04:39 -0500

    raw dataframes

--- src/main/scala/zillow/DataFrameBuilder.scala
+++ src/main/scala/zillow/DataFrameBuilder.scala
@@ -1,6 +1,6 @@
 package zillow
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object DataFrameBuilder {
 
@@ -9,4 +9,20 @@ object DataFrameBuilder {
     .master("local[4]")
     .getOrCreate()
 
+  spark.sparkContext.setLogLevel("WARN")
+
+  //Rent
+  val rentPrice1BedDF: DataFrame = spark.read.option("header", true).csv("data/City_MedianRentalPrice_1Bedroom.csv").toDF()
+  val rentPrice2BedDF: DataFrame = spark.read.option("header", true).csv("data/City_MedianRentalPrice_2Bedroom.csv").toDF()
+  val rentPrice3BedDF: DataFrame = spark.read.option("header", true).csv("data/City_MedianRentalPrice_3Bedroom.csv").toDF()
+  val rentPrice4BedDF: DataFrame = spark.read.option("header", true).csv("data/City_MedianRentalPrice_4Bedroom.csv").toDF()
+  val rentPrice5OrMoreBedDF: DataFrame = spark.read.option("header", true).csv("data/City_MedianRentalPrice_5BedroomOrMore.csv").toDF()
+  val rentPriceStudioDF: DataFrame = spark.read.option("header", true).csv("data/City_MedianRentalPrice_Studio.csv").toDF()
+
+  //Homes
+  val homePrice1BedDF: DataFrame = spark.read.option("header", true).csv("data/City_Zhvi_1bedroom.csv").toDF()
+  val homePrice2BedDF: DataFrame = spark.read.option("header", true).csv("data/City_Zhvi_2bedroom.csv").toDF()
+  val homePrice3BedDF: DataFrame = spark.read.option("header", true).csv("data/City_Zhvi_3bedroom.csv").toDF()
+  val homePrice4BedDF: DataFrame = spark.read.option("header", true).csv("data/City_Zhvi_4bedroom.csv").toDF()
+  val homePrice5OrMoreBedDF: DataFrame = spark.read.option("header", true).csv("data/City_Zhvi_5BedroomOrMore.csv").toDF()
 }
--- src/main/scala/zillow/Runner.scala
+++ src/main/scala/zillow/Runner.scala
@@ -1,5 +1,18 @@
 package zillow
 
+
 object Runner extends App {
   println("Hello World")
+  DataFrameBuilder.rentPrice1BedDF.explain()
+  DataFrameBuilder.rentPrice2BedDF.explain()
+  DataFrameBuilder.rentPrice3BedDF.explain()
+  DataFrameBuilder.rentPrice4BedDF.explain()
+  DataFrameBuilder.rentPrice5OrMoreBedDF.explain()
+  DataFrameBuilder.rentPriceStudioDF.explain()
+
+  DataFrameBuilder.homePrice1BedDF.explain()
+  DataFrameBuilder.homePrice2BedDF.explain()
+  DataFrameBuilder.homePrice3BedDF.explain()
+  DataFrameBuilder.homePrice4BedDF.explain()
+  DataFrameBuilder.homePrice5OrMoreBedDF.explain()
 }
