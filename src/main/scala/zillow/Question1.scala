3de755a67d93d57f54f7824e53bc0de79ff2f87a
srtidd176 <riverstone98@gmail.com>
2021-01-28 11:52:26 -0500

    Q1 Done

--- src/main/scala/zillow/Question1.scala
+++ src/main/scala/zillow/Question1.scala
@@ -20,7 +20,12 @@ object Question1 extends java.io.Serializable{
     /*
     NYC, Los Angeles, Chicago, Houston, Pheonix, Philadelphia, San Antonio, San Diego, Dallas, San Jose
      */
-    for(i <- 0 to 0) {
+
+    //Register a user defined function to work on spark DF
+    val avgPercChangeUDF = udf((array: Seq[String]) => avgPercChange(array.toArray))
+    spark.udf.register("avgPercChangeUDF", avgPercChangeUDF)
+
+    for(i <- 0 to 5) {
 
       //Get specific high population density regions
       val columns = DataFrameBuilder.getRent(i).columns
@@ -30,36 +35,24 @@ object Question1 extends java.io.Serializable{
         "CountyName"), monthColumns).toArray
 
       val cityDF = DataFrameBuilder.getRent(i).select(finalColumns.head, finalColumns.tail: _*).
-        filter($"RegionName" === "New York" ||
-          $"RegionName" === "Los Angeles" || $"RegionName" === "Chicago" ||
-          $"RegionName" === "Houston" || $"RegionName" === "Pheonix" ||
-          $"RegionName" === "Philadelphia" || $"RegionName" === "San Antonio" ||
-          $"RegionName" === "San Diego" || $"RegionName" === "Dallas" ||
-          $"RegionName" === "San Jose")
-      cityDF.show()
+        filter(($"RegionName" === "New York" && $"State" === "NY") ||
+          ($"RegionName" === "Los Angeles" && $"State" === "CA") || ($"RegionName" === "Chicago" && $"State" === "IL") ||
+          ($"RegionName" === "Houston" && $"State" === "TX") || ($"RegionName" === "Pheonix" && $"State" === "AZ") ||
+          ($"RegionName" === "Philadelphia"  && $"State" ==="PA") || ($"RegionName" === "San Antonio" && $"State" === "TX") ||
+          ($"RegionName" === "San Diego" && $"State" === "CA") || ($"RegionName" === "Dallas" && $"State" === "TX") ||
+          ($"RegionName" === "San Jose" && $"State" === "CA"))
 
-      //Group price columns in an array
-      val cityRDD = cityDF.rdd
-      val newRDD: RDD[Row] = cityRDD.map(row => Row(row.get(0),row.get(1),row.get(2),row.get(3),
-        Array(row.get(4), row.get(5), row.get(6), row.get(7), row.get(8), row.get(9), row.get(10)
-        , row.get(11), row.get(12), row.get(13))))
+      val df = cityDF.select($"RegionName", $"State", $"Metro", $"CountyName", functions.array(monthColumns.head, monthColumns.tail: _*) as "YearlyPrice")
 
-      val schema = new StructType()
-        .add(StructField("RegionName", StringType, true))
-        .add(StructField("State", StringType, true))
-        .add(StructField("Metro", StringType, true))
-        .add(StructField("CountyName", StringType, true))
-        .add(StructField("YearlyPrice", ArrayType(StringType, true), true))
-
-      val df = spark.createDataFrame(newRDD, schema)
-
-      //Register a user defined function to work on spark DF
-      val avgPercChangeUDF = udf((array: Seq[String]) => avgPercChange(array.toArray))
-      spark.udf.register("avgPercChangeUDF", avgPercChangeUDF)
-
+      if(i == 0){
+        println("Studio Rent: ")
+      }
+      else{
+        println(s"$i Bedroom Rent: ")
+      }
       df.withColumn("YearlyPrice", avgPercChangeUDF($"YearlyPrice"))
         .withColumnRenamed("YearlyPrice","AvgYearlyPercentChange")
-        .orderBy(functions.asc("AvgYearlyPercentChange"))show()
+        .orderBy(functions.asc("AvgYearlyPercentChange")).show()
     }
 
 
@@ -70,22 +63,32 @@ object Question1 extends java.io.Serializable{
     NYC, Los Angeles, Chicago, Houston, Pheonix, Philadelphia, San Antonio, San Diego, Dallas, San Jose
      */
 
+    val avgPercChangeUDF = udf((array: Seq[String]) => avgPercChange(array.toArray))
+    spark.udf.register("avgPercChangeUDF", avgPercChangeUDF)
+
     for(i <- 1 to 5) {
+
+      //Get specific high population density regions
       val columns = DataFrameBuilder.getHome(i).columns
-      val regex = "\\w.*-12".r
+      val regex = "\\w.*-01-".r
       val monthColumns = columns.filter(s => regex.findFirstIn(s).isDefined)
       val finalColumns = ArrayBuffer.concat(Array("RegionName", "State", "Metro",
         "CountyName"), monthColumns).toArray
 
       val cityDF = DataFrameBuilder.getHome(i).select(finalColumns.head, finalColumns.tail: _*).
-        filter($"RegionName" === "New York" ||
-          $"RegionName" === "Los Angeles" || $"RegionName" === "Chicago" ||
-          $"RegionName" === "Houston" || $"RegionName" === "Pheonix" ||
-          $"RegionName" === "Philadelphia" || $"RegionName" === "San Antonio" ||
-          $"RegionName" === "San Diego" || $"RegionName" === "Dallas" ||
-          $"RegionName" === "San Jose")
-      cityDF.show()
+        filter(($"RegionName" === "New York" && $"State" === "NY") ||
+          ($"RegionName" === "Los Angeles" && $"State" === "CA") || ($"RegionName" === "Chicago" && $"State" === "IL") ||
+          ($"RegionName" === "Houston" && $"State" === "TX") || ($"RegionName" === "Pheonix" && $"State" === "AZ") ||
+          ($"RegionName" === "Philadelphia"  && $"State" ==="PA") || ($"RegionName" === "San Antonio" && $"State" === "TX") ||
+          ($"RegionName" === "San Diego" && $"State" === "CA") || ($"RegionName" === "Dallas" && $"State" === "TX") ||
+          ($"RegionName" === "San Jose" && $"State" === "CA"))
 
+      val df = cityDF.select($"RegionName", $"State", $"Metro", $"CountyName", functions.array(monthColumns.head, monthColumns.tail: _*) as "YearlyPrice")
+
+      println(s"${i} Bedroom Homes: ")
+      df.withColumn("YearlyPrice", avgPercChangeUDF($"YearlyPrice"))
+        .withColumnRenamed("YearlyPrice","AvgYearlyPercentChange")
+        .orderBy(functions.asc("AvgYearlyPercentChange")).show()
     }
   }
 
--- src/main/scala/zillow/Runner.scala
+++ src/main/scala/zillow/Runner.scala
@@ -3,5 +3,5 @@ package zillow
 
 object Runner extends App {
 Question1.Q1Rent()
-
+Question1.Q1Home()
 }
