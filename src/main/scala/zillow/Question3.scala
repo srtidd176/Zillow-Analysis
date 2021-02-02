4da0f9e329714903330328808c2905edb942f217
Riztech16 <rizvi_syed101@outlook.com>
2021-02-01 20:00:09 -0500

    Refactoringbutfinished

--- src/main/scala/zillow/Question3.scala
+++ src/main/scala/zillow/Question3.scala
@@ -1,8 +1,8 @@
 package zillow
 
-import org.apache.spark.sql.functions.{avg, bround, col, collect_list, concat, concat_ws, expr, length, lit, monotonically_increasing_id, regexp_extract, regexp_replace}
+import org.apache.spark.sql.functions.{aggregate, avg, bround, col, collect_list, concat, concat_ws, expr, length, lit, min, monotonically_increasing_id, regexp_extract, regexp_replace}
+import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
-
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -223,43 +223,126 @@ object Question3 extends java.io.Serializable {
   }
 
   def Q3Analysis():Unit={
+    val yearly_ratio_list = Seq("2019_PI_Ratio", "2018_PI_Ratio",
+      "2017_PI_Ratio", "2016_PI_Ratio",
+      "2015_PI_Ratio","2014_PI_Ratio")
 
+    val RentDF_names = Seq("Studio","1 bedroom","2 bedrooms",
+      "3 bedrooms","4 bedrooms","5 or more bedrooms")
+
+    val HomeDF_names = Seq("1 bedroom","2 bedrooms",
+      "3 bedrooms","4 bedrooms","5 or more bedrooms")
+
+    val schema = new StructType()
+      .add("State",StringType)
+      .add("Year",StringType)
+      .add("PI_ratio",DoubleType)
+/*
     var RentPriceStudioDF = Q3Rent(0)
     var RentPrice1BedDF  = Q3Rent(1)
-
-    /*
     var RentPrice2BedDF  = Q3Rent(2)
     var RentPrice3BedDF  = Q3Rent(3)
     var RentPrice4BedDF  = Q3Rent(4)
     var RentPrice5OrMoreBedDF = Q3Rent(5)
 
+ */
+
     var HomePrice1BedDF  = Q3Home(1)
+    /*
     var HomePrice2BedDF  = Q3Home(2)
     var HomePrice3BedDF  = Q3Home(3)
     var HomePrice4BedDF  = Q3Home(4)
     var HomePrice5OrMoreBedDF  = Q3Home(5)
+
      */
 
     var IncomeDF = Q3Income()
-
+/*
     RentPriceStudioDF = RentPriceStudioDF.join(IncomeDF,Seq("State"),"inner")
     RentPrice1BedDF = RentPrice1BedDF.join(IncomeDF, Seq("State"), "inner")
-    val RentDFs = Seq(RentPriceStudioDF, RentPrice1BedDF)
+    RentPrice2BedDF = RentPrice2BedDF.join(IncomeDF, Seq("State"), "inner")
+    RentPrice3BedDF = RentPrice3BedDF.join(IncomeDF, Seq("State"), "inner")
+    RentPrice4BedDF = RentPrice4BedDF.join(IncomeDF, Seq("State"), "inner")
+    RentPrice5OrMoreBedDF = RentPrice5OrMoreBedDF.join(IncomeDF, Seq("State"), "inner")
 
+ */
+    HomePrice1BedDF = HomePrice1BedDF.join(IncomeDF, Seq("State"), "inner")
+    /*
+    HomePrice2BedDF = HomePrice2BedDF.join(IncomeDF, Seq("State"), "inner")
+    HomePrice3BedDF = HomePrice3BedDF.join(IncomeDF, Seq("State"), "inner")
+    HomePrice4BedDF = HomePrice4BedDF.join(IncomeDF, Seq("State"), "inner")
+    HomePrice5OrMoreBedDF = HomePrice5OrMoreBedDF.join(IncomeDF, Seq("State"), "inner")
+
+    val RentDFs = Seq(RentPriceStudioDF,RentPrice1BedDF,
+      RentPrice2BedDF,RentPrice3BedDF,
+      RentPrice4BedDF,RentPrice5OrMoreBedDF)
+     */
+    val HomeDFs = Seq(HomePrice1BedDF)
+
+    var Rent_i = 0
+    var Home_i = 0
+/*
     for(df_name <- RentDFs){
-      val df = df_name
-        .withColumn("2019_PI_Ratio",bround((df_name("2019_Rent")/(df_name("2019_Income")/12))*100,0))
-        .withColumn("2018_PI_Ratio",bround((df_name("2018_Rent")/(df_name("2018_Income")/12))*100,0))
-        .withColumn("2017_PI_Ratio",bround((df_name("2017_Rent")/(df_name("2017_Income")/12))*100,0))
-        .withColumn("2016_PI_Ratio",bround((df_name("2016_Rent")/(df_name("2016_Income")/12))*100,0))
-        .withColumn("2015_PI_Ratio",bround((df_name("2015_Rent")/(df_name("2015_Income")/12))*100,0))
-        .withColumn("2014_PI_Ratio",bround((df_name("2014_Rent")/(df_name("2014_Income")/12))*100,0))
 
+      val name = RentDF_names(Rent_i)
+
+      val df = df_name
+        .withColumn("2019_PI_Ratio", bround((df_name("2019_Rent") / (df_name("2019_Income") / 12)) * 100, 0))
+        .withColumn("2018_PI_Ratio", bround((df_name("2018_Rent") / (df_name("2018_Income") / 12)) * 100, 0))
+        .withColumn("2017_PI_Ratio", bround((df_name("2017_Rent") / (df_name("2017_Income") / 12)) * 100, 0))
+        .withColumn("2016_PI_Ratio", bround((df_name("2016_Rent") / (df_name("2016_Income") / 12)) * 100, 0))
+        .withColumn("2015_PI_Ratio", bround((df_name("2015_Rent") / (df_name("2015_Income") / 12)) * 100, 0))
+        .withColumn("2014_PI_Ratio", bround((df_name("2014_Rent") / (df_name("2014_Income") / 12)) * 100, 0))
+
+      println(s"Average of Median Rental Price for ${name}")
       df.show()
+
+      for(ratio_year <- yearly_ratio_list){
+        println(s"${ratio_year.slice(0,4)} affordable state for rent among top 10 populous state")
+        df.select("State",ratio_year)
+          .where(df(ratio_year)=!=0)
+          .withColumnRenamed(ratio_year,"PI_ratio")
+          .orderBy("PI_ratio")
+          .limit(1)
+          .show()
+      }
+      Rent_i += 1
     }
 
+
+ */
+    for(df_name <- HomeDFs){
+
+      val name = HomeDF_names(Home_i)
+
+      val df = df_name
+        .withColumn("2019_PI_Ratio", bround((df_name("2019_Home") / df_name("2019_Income")) * 100, 0))
+        .withColumn("2018_PI_Ratio", bround((df_name("2018_Home") / df_name("2018_Income")) * 100, 0))
+        .withColumn("2017_PI_Ratio", bround((df_name("2017_Home") / df_name("2017_Income")) * 100, 0))
+        .withColumn("2016_PI_Ratio", bround((df_name("2016_Home") / df_name("2016_Income")) * 100, 0))
+        .withColumn("2015_PI_Ratio", bround((df_name("2015_Home") / df_name("2015_Income")) * 100, 0))
+        .withColumn("2014_PI_Ratio", bround((df_name("2014_Home") / df_name("2014_Income")) * 100, 0))
+
+      println(s"Average of Median House Price for ${name}")
+      df.show()
+
+      var df_temp1 = spark.createDataFrame(spark.sparkContext.emptyRDD[Row],schema)
+      println("Affordable state for house among top 10 populous state by year")
+      for(ratio_year <- yearly_ratio_list){
+
+        var df_temp2 = df.select("State",ratio_year)
+          .withColumn("Year",lit(ratio_year.slice(0,4)))
+          .where(df(ratio_year)=!=0)
+          .withColumnRenamed(ratio_year,"PI_ratio")
+          .orderBy("PI_ratio")
+          .select("Year","State","PI_ratio")
+          .limit(1)
+
+        df_temp1 = df_temp1.union(df_temp2)
+      }
+      df_temp1.show()
+      Home_i += 1
+    }
   }
-
-
 
 }
